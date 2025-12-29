ab3c21c5acb8038bcbd1b58d4707634398b62fe8
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-12-29 16:18:26 +0800

    add addtional information

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html>
 <head>
     <title>Laravel 10 CRUD Application - ItSolutionStuff.com - Snacks Library</title>
@@ -9,6 +8,8 @@
 <div class="container">
     @yield('content')
 </div>
-<footer class="flex text-center">Created by RMPlaysMC YT <br> This wasn't mine however I did a version which tells about snacks well <a href="https://www.itsolutionstuff.com/post/laravel-10-crud-application-example-tutorialexample.html">grabe a source here</a></footer>
+<footer class="flex text-center">Created by RMPlaysMC YT <br> This wasn't mine however I did a version which tells about snacks well <a href="https://www.itsolutionstuff.com/post/laravel-10-crud-application-example-tutorialexample.html">grabe a source here</a> <br>
+<a href="https://github.com/RMPlaysMCYT/SnacksLibraryAPI"></a>
+</footer>
 </body>
 </html>
